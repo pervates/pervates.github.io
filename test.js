64240ec6c8e780cb5109500e814f37b60d7e6448
pervates <warwhoop70960@gmail.com>
2016-05-14 01:21:24 +0800

    0513 change 1

--- test.js
+++ test.js
@@ -1,1 +1,37 @@
-console.log(“Hello World!”);
+
+
+
+console.log("Hello World!");
+
+
+var count = 1;
+
+count = count + 1;
+console.log(count);
+
+count += 1 ;
+console.log(count);
+
+
+var a,b,c ;
+
+a = 3;
+a = a++;
+a = 3;
+b = a++;
+a = 3
+c = ++a;
+
+console.log("a = " + a);
+console.log("b = " + b);
+console.log("c = " + c);
+
+
+var r;
+var pi = 3.14;
+var cirArea = function(r,pi)
+{
+	return r * r * pi;
+};
+
+console.log("cirArea = " + cirArea(3,pi));
